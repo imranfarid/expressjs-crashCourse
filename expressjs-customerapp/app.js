105dd59adbf97474fcbfad92ec7bd22230ed7b76
imranfarid <imran.cyborg@gmail.com>
2017-06-24 03:46:26 +0600

    passing data

--- expressjs-customerapp/app.js
+++ expressjs-customerapp/app.js
@@ -24,12 +24,32 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+var users = [
+    {
+        id: 1,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'johndoe@gmail.com'
+    },
+    {
+        id: 2,
+        first_name: 'Bob',
+        last_name: 'Dilan',
+        email: 'bobdilan@gmail.com'
+    },
+    {
+        id: 3,
+        first_name: 'James',
+        last_name: 'Hetfield',
+        email: 'jameshatefield@gmail.com'
+    }
+]
 
 app.get('/', function(req, res){
     //res.send('Hello World!');
     res.render('index', {
-        title: 'Customers'
+        title: 'Customers',
+        users: users
     });
 });
 
