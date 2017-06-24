6128dda6f687c0e682ec229c24bbc5bd6851f5eb
imranfarid <imran.cyborg@gmail.com>
2017-06-24 08:15:54 +0600

    form & express-validator

--- expressjs-customerapp/app.js
+++ expressjs-customerapp/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -24,12 +25,39 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Set Static Path
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Global vars
+app.use(function(req, res, next){
+    res.locals.errors = null;
+    next();    
+})
+
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+
 var users = [
     {
         id: 1,
-        first_name: 'John',
-        last_name: 'Doe',
-        email: 'johndoe@gmail.com'
+        first_name: 'Bon',
+        last_name: 'Jovi',
+        email: 'bonjovi@gmail.com'
     },
     {
         id: 2,
@@ -52,6 +80,30 @@ app.get('/', function(req, res){
         users: users
     });
 });
+
+app.post('/users/add', function(req, res){
+    
+    req.checkBody('first_name', 'First Name is Required').notEmpty();
+    req.checkBody('last_name', 'Last Name is Required').notEmpty();
+    req.checkBody('email', 'Email is Required').notEmpty();
+    
+    var errors = req.validationErrors();
+
+    if(errors){
+        res.render('index', {
+            title: 'Customers',
+            users: users,
+            errors: errors
+        });
+    }else{
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+        }
+        console.log('SUCESS');
+    }
+})
 
 app.listen(3000, function(){
     console.log('Server started on port 3000...');
